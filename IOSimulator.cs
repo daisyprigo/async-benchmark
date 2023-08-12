5a1dd30e70fd7d1c571c9950ec9625cce295664e
pciszewski <pciszewski@microsoft.com>
2023-08-12 16:06:38 -0700

    V2 with simulated IO

--- IOSimulator.cs
+++ IOSimulator.cs
@@ -10,16 +10,68 @@ public class IOSimulator
     /// This controls how many files we will read concurrently, e.g. 3 different sizes of runs: 10, 100, 1000
     /// </summary>
     //[Params(10, 100, 1000)]
-    [Params(10)]
+    [Params(100)]
     public int ConcurrentOperationCount;
 
-    [Params(0, 100)]
-    public int ReadInterval;
+    [Params(100)]
+    public int MaxIntervalBetweenOperationsMs;
+
+    public int MinOperationDurationMs;
+
+    public int SimulatedProcessingTimeInIterations;
 
     [GlobalSetup]
     public void Setup()
     {
         testFileList = GetTestFileList(@"c:\windows\system32");
+    }
+
+    public void SimulateSyncIOOperations()
+    {
+        ExecuteConcurrentIO(SimulatedSyncIOOperation);
+    }
+    public void SimulateASyncIOOperations()
+    {
+        ExecuteConcurrentIO(SimulatedASyncIOOperation);
+    }
+    private async Task SimulatedSyncIOOperation()
+    {
+        // simulate some processing on the CPU
+        Thread.SpinWait(SimulatedProcessingTimeInIterations);
+        // simulate blocking the thread for an outbound IO call (e.g. reading form disk, calling an external process)
+        await Task.Run(() => Thread.Sleep(MinOperationDurationMs)).ConfigureAwait(false);
+        // simulate some post-processing on the CPU
+        Thread.SpinWait(SimulatedProcessingTimeInIterations);
+    }
+    private async Task SimulatedASyncIOOperation()
+    {
+        // simulate some processing on the CPU
+        Thread.SpinWait(SimulatedProcessingTimeInIterations);
+        // simulate making an outbound IO call (e.g. reading form disk, calling an external process) without blocking the thread
+        await Task.Delay(MinOperationDurationMs).ConfigureAwait(false);
+        // simulate some post-processing on the CPU
+        Thread.SpinWait(SimulatedProcessingTimeInIterations);
+    }
+
+   
+
+    private void ExecuteConcurrentIO(Func<Task> ioOperation)
+    {
+        var rand = new Random();
+        var runningTasks = new List<Task>(ConcurrentOperationCount);
+        for (int i = 0; i < ConcurrentOperationCount; i++)
+        {
+            var task = ioOperation();
+            runningTasks.Add(task);
+            // Wait a little bit between read operations, to simulate a distribution of incoming requests over time
+            if (MaxIntervalBetweenOperationsMs > 0)
+            {
+                Thread.Sleep(MaxIntervalBetweenOperationsMs);
+            }
+
+        }
+        // wait for all read operations to complete
+        Task.WaitAll(runningTasks.ToArray());
     }
 
     /// <summary>
@@ -33,6 +85,7 @@ public class IOSimulator
             return Task.Run(() => File.ReadAllBytes(file));
         });
     }
+
     /// <summary>
     /// Reads files concurrently, using ThreadPool threads, using an asynchronous read.
     /// </summary>
@@ -43,6 +96,7 @@ public class IOSimulator
     }
     private void ReadFilesConcurrently(Func<string, CancellationToken, Task<byte[]>> fileRead)
     {
+        var rand = new Random();
         var filesToLoad = GetRandomTestFileSample();
         var runningTasks = new List<Task>(ConcurrentOperationCount);
         foreach (var file in filesToLoad)
@@ -51,8 +105,11 @@ public class IOSimulator
             task.ConfigureAwait(false);
             runningTasks.Add(task);
             // Wait a little bit between read operations, to simulate a distribution of incoming requests over time
-            if (ReadInterval > 0)
-                Task.Delay(ReadInterval).Wait();
+            if (MaxIntervalBetweenOperationsMs > 0)
+            {
+                Task.Delay(rand.Next(MaxIntervalBetweenOperationsMs)).Wait();
+            }
+                
         }
         // wait for all read operations to complete
         Task.WaitAll(runningTasks.ToArray());
--- Program.cs
+++ Program.cs
@@ -2,14 +2,21 @@
 using BenchmarkDotNet.Running;
 using System.Security.Cryptography;
 
-BenchmarkRunner.Run<IOSimulator>();
+//BenchmarkRunner.Run<IOSimulator>();
 
-return;
+//return;
 
 var x = new IOSimulator();
 x.Setup();
-x.ConcurrentOperationCount = 100;
-x.ReadFilesSync();
+x.ConcurrentOperationCount = 10000;
+x.MaxIntervalBetweenOperationsMs = 0;
+x.MinOperationDurationMs = 5000;
+x.SimulatedProcessingTimeInIterations = 1000;
+
+Thread.Sleep(2000);
+
+
+x.SimulateSyncIOOperations();
 return;
 
 
